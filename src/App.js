c5a891ce8f34ad2f2f480bd2595810775995acf0
kidohy15 <kidohy307@gmail.com>
2024-01-12 23:04:38 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,53 @@ import logo from "./logo.svg";
 import "./App.css";
 import Customer from "./components/Customer";
 
-const customer = {
-  name: "홍길동",
-  birthday: "961222",
-  gender: "남자",
-  job: "대학생",
-};
+const customers = [
+  {
+    id: 1,
+    // image: 'http://via.placeholder.com/60x60',
+    image: "https://placebear.com/60/60",
+    name: "홍길동",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    // image: 'http://via.placeholder.com/60x60',
+    image: "https://placebear.com/60/60",
+    name: "이올린",
+    birthday: "201223",
+    gender: "여자",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    // image: 'http://via.placeholder.com/60x60',
+    image: "https://placebear.com/60/60",
+    name: "살라딘",
+    birthday: "241012",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
+    </div>
   );
 }
 
